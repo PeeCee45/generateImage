cb65b0deb6dddef6432e1203a4262c143eff28fe
Promise Ibeh <ibehpromise45@gmail.com>
2025-05-11 23:34:47 +0100

    Update generate-image.js

--- api/generate-image.js
+++ api/generate-image.js
@@ -1,38 +1,44 @@
 const nodeHtmlToImage = require('node-html-to-image');
 
+async function generate() {
+    try {
+      const html = `
+        <html>
+          <head>
+            <style>
+              body {
+                font-family: Arial, sans-serif;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                background: #f4f4f4;
+              }
+              h1 {
+                color: #333;
+              }
+            </style>
+          </head>
+          <body>
+            <h1>Hello from static HTML!</h1>
+          </body>
+        </html>
+      `;
+    
+      const image = await nodeHtmlToImage({
+        html,
+        encoding: 'base64'
+      });
+      
+      return `data:image/png;base64,${image}`;
+    } catch (error) {
+      return 'Image generation error:', error;
+    }
+}
 module.exports = async (req, res) => {
-  try {
-    const html = `
-      <html>
-        <head>
-          <style>
-            body {
-              font-family: Arial, sans-serif;
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              height: 100vh;
-              background: #f4f4f4;
-            }
-            h1 {
-              color: #333;
-            }
-          </style>
-        </head>
-        <body>
-          <h1>Hello from static HTML!</h1>
-        </body>
-      </html>
-    `;
-
-    const image = await nodeHtmlToImage({
-      html,
-      encoding: 'base64'
-    });
-
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json({
-      image: `data:image/png;base64,${image}`
+      image: generate()
     });
   } catch (err) {
     console.error('Error generating image:', err);
